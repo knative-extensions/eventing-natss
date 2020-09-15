63d8576385b974960508a5fcbdf991ec04b469a4
Scott Nichols <snichols@vmware.com>
2020-09-15 14:57:36 -0700

    mirgate eventing-contrib to be just natss

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -31,7 +31,6 @@ VERSION="master"
 FLOATING_DEPS=(
   "knative.dev/pkg@${VERSION}"
   "knative.dev/eventing@${VERSION}"
-  "knative.dev/serving@${VERSION}"
   "knative.dev/test-infra@${VERSION}"
 )
 
