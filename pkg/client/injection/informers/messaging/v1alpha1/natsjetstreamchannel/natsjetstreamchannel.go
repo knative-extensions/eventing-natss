9cb9e31f6809f874424a1b0caa682f265fc2202c
Knative Automation <automation@knative.team>
2021-08-19 02:05:16 +0000

    upgrade to latest dependencies
    bumping knative.dev/reconciler-test b61cc77...1e1e9f7:
      > 1e1e9f7 Update actions (# 232)
    bumping knative.dev/eventing 0c41e6d...4b18ab8:
      > 4b18ab8 Add myself to support roster (# 5640)
      > 5c08bd3 be more explicit about the semantic of ceOverrides (# 5641)
      > c183833 Resurrect source spec (# 5637)
      > 72fae58 Add trace exporting to mt broker (# 5634)
      > 41e36ee Update community files (# 5632)
      > 6296a81 Add tracing support to eventdisplay and heartbeats (# 5631)
    bumping knative.dev/pkg 21eb4c1...7b5ecbc:
      > 7b5ecbc Sketch out an alternate way of injecting clients/informers (# 2210)
      > 8c88fa2 Revert "Remove the unused file (# 1935)" (# 2213)
      > 50410e0 Use consistent case for "Deprecated" comments (# 2216)
      > 35bcd16 Drop redundant pointers and decoders (# 2211)
      > bf176d5 Sink two codegen passes below loop. (# 2209)
      > 2c8a7b6 Update community files (# 2208)
      > 23b0147 Update actions (# 2206)
    bumping knative.dev/hack e28525d...815cd31:
      > 815cd31 Stop performing pre-releases (# 77)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- pkg/client/injection/informers/messaging/v1alpha1/natsjetstreamchannel/natsjetstreamchannel.go
+++ pkg/client/injection/informers/messaging/v1alpha1/natsjetstreamchannel/natsjetstreamchannel.go
@@ -21,8 +21,15 @@ package natsjetstreamchannel
 import (
 	context "context"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	labels "k8s.io/apimachinery/pkg/labels"
+	cache "k8s.io/client-go/tools/cache"
+	apismessagingv1alpha1 "knative.dev/eventing-natss/pkg/apis/messaging/v1alpha1"
+	versioned "knative.dev/eventing-natss/pkg/client/clientset/versioned"
 	v1alpha1 "knative.dev/eventing-natss/pkg/client/informers/externalversions/messaging/v1alpha1"
+	client "knative.dev/eventing-natss/pkg/client/injection/client"
 	factory "knative.dev/eventing-natss/pkg/client/injection/informers/factory"
+	messagingv1alpha1 "knative.dev/eventing-natss/pkg/client/listers/messaging/v1alpha1"
 	controller "knative.dev/pkg/controller"
 	injection "knative.dev/pkg/injection"
 	logging "knative.dev/pkg/logging"
@@ -30,6 +37,7 @@ import (
 
 func init() {
 	injection.Default.RegisterInformer(withInformer)
+	injection.Dynamic.RegisterDynamicInformer(withDynamicInformer)
 }
 
 // Key is used for associating the Informer inside the context.Context.
@@ -41,6 +49,11 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 	return context.WithValue(ctx, Key{}, inf), inf.Informer()
 }
 
+func withDynamicInformer(ctx context.Context) context.Context {
+	inf := &wrapper{client: client.Get(ctx)}
+	return context.WithValue(ctx, Key{}, inf)
+}
+
 // Get extracts the typed informer from the context.
 func Get(ctx context.Context) v1alpha1.NatsJetStreamChannelInformer {
 	untyped := ctx.Value(Key{})
@@ -49,4 +62,45 @@ func Get(ctx context.Context) v1alpha1.NatsJetStreamChannelInformer {
 			"Unable to fetch knative.dev/eventing-natss/pkg/client/informers/externalversions/messaging/v1alpha1.NatsJetStreamChannelInformer from context.")
 	}
 	return untyped.(v1alpha1.NatsJetStreamChannelInformer)
+}
+
+type wrapper struct {
+	client versioned.Interface
+
+	namespace string
+}
+
+var _ v1alpha1.NatsJetStreamChannelInformer = (*wrapper)(nil)
+var _ messagingv1alpha1.NatsJetStreamChannelLister = (*wrapper)(nil)
+
+func (w *wrapper) Informer() cache.SharedIndexInformer {
+	return cache.NewSharedIndexInformer(nil, &apismessagingv1alpha1.NatsJetStreamChannel{}, 0, nil)
+}
+
+func (w *wrapper) Lister() messagingv1alpha1.NatsJetStreamChannelLister {
+	return w
+}
+
+func (w *wrapper) NatsJetStreamChannels(namespace string) messagingv1alpha1.NatsJetStreamChannelNamespaceLister {
+	return &wrapper{client: w.client, namespace: namespace}
+}
+
+func (w *wrapper) List(selector labels.Selector) (ret []*apismessagingv1alpha1.NatsJetStreamChannel, err error) {
+	lo, err := w.client.MessagingV1alpha1().NatsJetStreamChannels(w.namespace).List(context.TODO(), v1.ListOptions{
+		LabelSelector: selector.String(),
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
+	if err != nil {
+		return nil, err
+	}
+	for idx := range lo.Items {
+		ret = append(ret, &lo.Items[idx])
+	}
+	return ret, nil
+}
+
+func (w *wrapper) Get(name string) (*apismessagingv1alpha1.NatsJetStreamChannel, error) {
+	return w.client.MessagingV1alpha1().NatsJetStreamChannels(w.namespace).Get(context.TODO(), name, v1.GetOptions{
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
 }
--- pkg/client/injection/informers/messaging/v1beta1/natsschannel/natsschannel.go
+++ pkg/client/injection/informers/messaging/v1beta1/natsschannel/natsschannel.go
@@ -21,8 +21,15 @@ package natsschannel
 import (
 	context "context"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	labels "k8s.io/apimachinery/pkg/labels"
+	cache "k8s.io/client-go/tools/cache"
+	apismessagingv1beta1 "knative.dev/eventing-natss/pkg/apis/messaging/v1beta1"
+	versioned "knative.dev/eventing-natss/pkg/client/clientset/versioned"
 	v1beta1 "knative.dev/eventing-natss/pkg/client/informers/externalversions/messaging/v1beta1"
+	client "knative.dev/eventing-natss/pkg/client/injection/client"
 	factory "knative.dev/eventing-natss/pkg/client/injection/informers/factory"
+	messagingv1beta1 "knative.dev/eventing-natss/pkg/client/listers/messaging/v1beta1"
 	controller "knative.dev/pkg/controller"
 	injection "knative.dev/pkg/injection"
 	logging "knative.dev/pkg/logging"
@@ -30,6 +37,7 @@ import (
 
 func init() {
 	injection.Default.RegisterInformer(withInformer)
+	injection.Dynamic.RegisterDynamicInformer(withDynamicInformer)
 }
 
 // Key is used for associating the Informer inside the context.Context.
@@ -41,6 +49,11 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 	return context.WithValue(ctx, Key{}, inf), inf.Informer()
 }
 
+func withDynamicInformer(ctx context.Context) context.Context {
+	inf := &wrapper{client: client.Get(ctx)}
+	return context.WithValue(ctx, Key{}, inf)
+}
+
 // Get extracts the typed informer from the context.
 func Get(ctx context.Context) v1beta1.NatssChannelInformer {
 	untyped := ctx.Value(Key{})
@@ -49,4 +62,45 @@ func Get(ctx context.Context) v1beta1.NatssChannelInformer {
 			"Unable to fetch knative.dev/eventing-natss/pkg/client/informers/externalversions/messaging/v1beta1.NatssChannelInformer from context.")
 	}
 	return untyped.(v1beta1.NatssChannelInformer)
+}
+
+type wrapper struct {
+	client versioned.Interface
+
+	namespace string
+}
+
+var _ v1beta1.NatssChannelInformer = (*wrapper)(nil)
+var _ messagingv1beta1.NatssChannelLister = (*wrapper)(nil)
+
+func (w *wrapper) Informer() cache.SharedIndexInformer {
+	return cache.NewSharedIndexInformer(nil, &apismessagingv1beta1.NatssChannel{}, 0, nil)
+}
+
+func (w *wrapper) Lister() messagingv1beta1.NatssChannelLister {
+	return w
+}
+
+func (w *wrapper) NatssChannels(namespace string) messagingv1beta1.NatssChannelNamespaceLister {
+	return &wrapper{client: w.client, namespace: namespace}
+}
+
+func (w *wrapper) List(selector labels.Selector) (ret []*apismessagingv1beta1.NatssChannel, err error) {
+	lo, err := w.client.MessagingV1beta1().NatssChannels(w.namespace).List(context.TODO(), v1.ListOptions{
+		LabelSelector: selector.String(),
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
+	if err != nil {
+		return nil, err
+	}
+	for idx := range lo.Items {
+		ret = append(ret, &lo.Items[idx])
+	}
+	return ret, nil
+}
+
+func (w *wrapper) Get(name string) (*apismessagingv1beta1.NatssChannel, error) {
+	return w.client.MessagingV1beta1().NatssChannels(w.namespace).Get(context.TODO(), name, v1.GetOptions{
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
 }
