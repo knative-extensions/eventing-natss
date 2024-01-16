98f6511e8df339f3c1322145d947026873c64f31
astelmashenko <andrey.stelmashenko@gmail.com>
2024-01-16 18:34:52 +0200

    backported pkg files from release-1.10

--- pkg/channel/jetstream/utils/consumerconfig.go
+++ pkg/channel/jetstream/utils/consumerconfig.go
@@ -17,8 +17,14 @@ limitations under the License.
 package utils
 
 import (
+	"math"
+	"time"
+
 	"github.com/nats-io/nats.go"
+	"github.com/rickb777/date/period"
 	"knative.dev/eventing-natss/pkg/apis/messaging/v1alpha1"
+	v1 "knative.dev/eventing/pkg/apis/duck/v1"
+	"knative.dev/eventing/pkg/kncloudevents"
 )
 
 func ConvertDeliverPolicy(in v1alpha1.DeliverPolicy, def nats.DeliverPolicy) nats.DeliverPolicy {
@@ -47,4 +53,107 @@ func ConvertReplayPolicy(in v1alpha1.ReplayPolicy, def nats.ReplayPolicy) nats.R
 	}
 
 	return def
+}
+
+func CalcRequestDeadline(msg *nats.Msg, ackWait time.Duration) time.Time {
+	const jitter = time.Millisecond * 200
+
+	// if previous deliveries were explicitly nacked earlier than the deadline, then our actual deadline will be earlier
+	// than the deadline above
+	ackDeadlineFromNow := time.Now().Add(ackWait).Add(-jitter)
+
+	meta, err := msg.Metadata()
+	if err != nil {
+		return ackDeadlineFromNow
+	}
+
+	// if each delivery has timed out, then multiplying the number of deliveries by the ack wait will give us the
+	// duration from publish which this attempt will be ack-waited
+	ackDurationFromPublish := time.Duration(meta.NumDelivered) * ackWait
+
+	// the deadline is the published timestamp plus our duration calculated above
+	deadline := meta.Timestamp.Add(ackDurationFromPublish).Add(-jitter)
+
+	if deadline.After(ackDeadlineFromNow) {
+		deadline = ackDeadlineFromNow
+	}
+	return deadline
+}
+
+func CalcRequestTimeout(msg *nats.Msg, ackWait time.Duration) time.Duration {
+	const jitter = time.Millisecond * 200
+
+	// if previous deliveries were explicitly nacked earlier than the deadline, then our actual deadline will be earlier
+	// than the deadline above
+	ackDeadlineFromNow := ackWait - jitter
+
+	meta, err := msg.Metadata()
+	if err != nil {
+		return ackDeadlineFromNow
+	}
+
+	// if each delivery has timed out, then multiplying the number of deliveries by the ack wait will give us the
+	// duration from publish which this attempt will be ack-waited
+	ackDurationFromPublish := time.Duration(meta.NumDelivered) * ackWait
+
+	// the deadline is the published timestamp plus our duration calculated above
+	deadline := ackDurationFromPublish - jitter
+
+	if deadline > ackDeadlineFromNow {
+		deadline = ackDeadlineFromNow
+	}
+	return deadline
+}
+
+func CalculateNakDelayForRetryNumber(attemptNum int, config *kncloudevents.RetryConfig) time.Duration {
+	backoff, backoffDelay := parseBackoffFuncAndDelay(config)
+	return backoff(attemptNum, backoffDelay)
+}
+
+type backoffFunc func(attemptNum int, delayDuration time.Duration) time.Duration
+
+func CalculateAckWaitAndBackoffDelays(config *kncloudevents.RetryConfig) (time.Duration, []time.Duration) {
+	var delays = make([]time.Duration, config.RetryMax)
+	var totalDelays = config.RequestTimeout * time.Duration(config.RetryMax)
+	// 1 second jitter
+	const jitter = 1 * time.Second
+
+	backoff, backoffDelay := parseBackoffFuncAndDelay(config)
+
+	for i := 0; i < config.RetryMax; i++ {
+		var nextDelay time.Duration
+		if i == 0 {
+			// the first backoff should be just request timeout + jitter
+			nextDelay = 0
+		} else {
+			nextDelay = backoff(i-1, backoffDelay)
+		}
+
+		totalDelays += nextDelay
+		delays[i] = nextDelay + config.RequestTimeout + jitter
+	}
+	return totalDelays, delays
+}
+
+func LinearBackoff(attemptNum int, delayDuration time.Duration) time.Duration {
+	return delayDuration * time.Duration(attemptNum)
+}
+
+func ExpBackoff(attemptNum int, delayDuration time.Duration) time.Duration {
+	return delayDuration * time.Duration(math.Exp2(float64(attemptNum)))
+}
+
+func parseBackoffFuncAndDelay(config *kncloudevents.RetryConfig) (backoffFunc, time.Duration) {
+	var backoff backoffFunc
+	switch *config.BackoffPolicy {
+	case v1.BackoffPolicyExponential:
+		backoff = ExpBackoff
+	case v1.BackoffPolicyLinear:
+		backoff = LinearBackoff
+	}
+	// it should be validated at this point
+	delay, _ := period.Parse(*config.BackoffDelay)
+	backoffDelay, _ := delay.Duration()
+
+	return backoff, backoffDelay
 }
